4816cb6e1c2df573dd0b802cb51751f42b62ec24
Paulina <paulinasfielek@gmail.com>
2023-12-13 12:48:20 -0800

    added styling, set authentication button to show only when needed

--- src/components/authenticate.jsx
+++ src/components/authenticate.jsx
@@ -3,8 +3,11 @@ import { useState } from "react"
 export default function Authenticate ({token}) {
     const [error, setError] = useState(null)
     const [success, setSuccess] = useState(null)
+    const [data, setData] = useState({})
 
     async function handleClick () {
+        setError(null)
+        setSuccess(null)
         try {
             const response = await fetch('https://fsa-jwt-practice.herokuapp.com/authenticate', 
               { 
@@ -16,17 +19,26 @@ export default function Authenticate ({token}) {
               })
               const result = await response.json()
               setSuccess(result.message)
+              console.log(result)
+              setData(result.data.username)
         }
         catch (error) {setError(error.message)}
     }
-
+    
+    if (token) {
     return (
+        
         <div>
         <h3> Authenticate </h3> 
-        {error && <p> {error} </p> }
-        {success && <p> {success} </p>}
+        {error &&  (<p className ="error"> {error} </p>) }
+        {success && success !== "jwt malformed" && 
+            (<div> <p className ="success"> {success} </p> <p className="info"> Your username is: <em> {data.username} </em> </p> </div>) }
+         
         
         <button onClick={handleClick}> Authenticate Token </button>
         </div>
     )
+    } else {
+        return <> </>
+    }
 }
--- src/components/signupform.jsx
+++ src/components/signupform.jsx
@@ -6,11 +6,13 @@ export default function SignupForm ({setToken, token}) {
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
     const [error, setError] = useState(null)
+    const [success, setSuccess] = useState(null)
     
     // functions
     async function handleSubmit(event) {
         event.preventDefault()
-        
+        setError(null)
+        if (username.length >= 8 && password.length >= 8) {
         try {
             const response = await fetch ("https://fsa-jwt-practice.herokuapp.com/signup", { 
                 method: "POST", 
@@ -25,12 +27,17 @@ export default function SignupForm ({setToken, token}) {
             const result = await response.json()
             console.log(result)
             setToken(result.token)
-            
+            setSuccess(result.success)
+            setUsername("")
+            setPassword("")
         }
         catch (error) {
             setError(error.message)
+        } } else if (username.length <8) {
+            setError("username needs to be 8 characters")
+        } else if (password.length <8) {
+            setError("password needs to be 8 characters")
         }
-        
     }
 
     // return
@@ -38,14 +45,15 @@ export default function SignupForm ({setToken, token}) {
         <>
         <form onSubmit={handleSubmit}> 
         <h2> Sign Up </h2> 
-        {error && <p> {error} </p> }
+        {error && <p className="error"> {error} </p> }
         <label> Username: 
-            <input value="username" onChange = {(event) => {setUsername(event.target.value)}}/>
+            <input value={username} onChange = {(event) => {setUsername(event.target.value)}}/>
         </label>
         <label> Password:
-            <input value="password" onChange = {(event) => {setPassword(event.target.value)}} />
+            <input value={password} onChange = {(event) => {setPassword(event.target.value)}} />
         </label> 
         <button type="submit" value="submit"> Submit </button> 
+        {success && <p className="success"> Please authenticate your username below </p>}
         </form>
         </> 
     )
